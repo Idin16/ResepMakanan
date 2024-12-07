6fe40aafc1b0b02a2db1bf9c2cc0890562b0b9ca
Idin16 <155719060+Idin16@users.noreply.github.com>
2024-12-08 05:34:03 +0800

    edit komentar_rating.py, pengguna.py
    menambahkan endpoint pada masing-masing bagian

--- resources/komentar_rating.py
+++ resources/komentar_rating.py
@@ -10,5 +10,52 @@ class KomentarRating(Resource):
     
 
     def post(self):
-        # kode kalo post request
-        return
+        data = request.json
+        nama = data.get('nama_resep')
+        pengguna = data.get('nama_pengguna')
+        tekskomentar = data.get('komentar')
+        nilairating = data.get('rating')
+
+        # Memanggil Prosedur
+        cursor = mysql.connection.cursor()
+        try:
+            cursor.execute("CALL TambahKomentarDanRating(%s,%s,%s,%s)", (nama,pengguna,tekskomentar,nilairating))
+        except Exception as e:
+            error_message = str(e)
+            if 'Resep tidak ditemukan' in error_message:
+                return jsonify({'message': f'Resep {nama} tidak ditemukan.'})
+            elif 'Pengguna tidak ditemukan' in error_message:
+                return jsonify({'message': f'Pengguna {pengguna} tidak ditemukan.'})
+            elif 'Anda sudah memberikan komentar untuk resep ini.' in error_message:
+                return jsonify({'message': f'Anda sudah memberikan komentar untuk resep ini.'})
+            elif 'Anda sudah memberikan rating untuk resep ini.' in error_message:
+                return jsonify({'message': f'Anda sudah memberikan rating untuk resep ini.'})
+            else:
+                return jsonify({'message': 'SystemError'})
+
+        return jsonify({'message': f'Komentar pada resep {nama} berhasil ditambahkan'})
+    
+    def delete(self):
+        data = request.json
+        nama = data.get('nama_resep')
+        pengguna = data.get('nama_pengguna')
+
+        if not nama or not pengguna:
+            return jsonify({'message': 'Nama resep dan nama pengguna tidak boleh kosong'})
+        
+        # Memanggil Prosdeur
+        cursor = mysql.connection.cursor()
+        try:
+            cursor.execute("CALL MenghapusKomentarRating(%s,%s)", (nama, pengguna))
+        except Exception as e:
+            error_message = str(e)
+            if 'Resep tidak ditemukan' in error_message:
+                return jsonify({'message': f'Resep {nama} tidak ditemukan.'})
+            elif 'Pengguna tidak ditemukan' in error_message:
+                return jsonify({'message': f'Pengguna {pengguna} tidak ditemukan.'})
+            else:
+                return jsonify({'message': 'SystemError'})
+
+        cursor.close()
+
+        return jsonify({'message': f'Komentar dari {pengguna} di resep {nama} berhasil dihapus'})
--- resources/pengguna.py
+++ resources/pengguna.py
@@ -4,10 +4,54 @@ from flask import jsonify, request, make_response
 from .import services
 
 class Pengguna(Resource):
-    def get(self):
-        return
-    
+    def get(self):     
+        cursor = mysql.connection.cursor()
+        cursor.execute("CALL LihatPengguna")
+        
+        data = cursor.fetchall()
+        data = services.cursor_to_json(cursor, data)
+        
+        return jsonify({'message': 'success', 'Pengguna': data})
 
     def post(self):
-        # kode kalo post request
-        return
+        data = request.json
+        nama = data.get('nama')
+        namaemail = data.get('email')
+        privatepassword = data.get('password')
+
+        # Memanggil Prosedur
+        cursor = mysql.connection.cursor()
+        try:
+            cursor.execute("CALL RegisterPengguna(%s,%s,%s)", (nama,namaemail,privatepassword))
+        except Exception as e:
+            error_message = str(e)
+            if 'Resep tidak ditemukan' in error_message:
+                return jsonify({'message': f'Username {nama} sudah digunakan.'})
+            elif 'Pengguna tidak ditemukan' in error_message:
+                return jsonify({'message': f'Email {namaemail} sudah digunakan.'})
+            else:
+                return jsonify({'message': 'SystemError'})
+
+        return jsonify({'message': f'Selamat! Akun {nama} berhasil dibuat'})
+    
+    def delete(self):
+        data = request.json
+        pengguna = data.get('nama_pengguna')
+
+        if not  pengguna:
+            return jsonify({'message': 'Nama pengguna tidak boleh kosong'})
+        
+        # Memanggil Prosdeur
+        cursor = mysql.connection.cursor()
+        try:
+            cursor.execute("CALL MenghapusPengguna(%s)", (pengguna, ))
+        except Exception as e:
+            error_message = str(e)
+            if 'Pengguna tidak ditemukan' in error_message:
+                return jsonify({'message': f'Pengguna {pengguna} tidak ditemukan.'})
+            else:
+                return jsonify({'message': 'SystemError'})
+
+        cursor.close()
+
+        return jsonify({'message': f'Pengguna {pengguna} berhasil dihapus'})
